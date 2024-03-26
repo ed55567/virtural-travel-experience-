b6a742d4e116460231b340c0bfd62300b23f261a
Emmanuel Daniels <edaniels@pltw.org>
2024-03-26 19:19:27 -0400

    add update to virtual.js

--- src/VirtualTour.js
+++ src/VirtualTour.js
@@ -1,11 +1,17 @@
 //VirtualTour.js
 import React from 'react';
+import PanoramaViewer from 'react-360-viewer';
 
 const VirtualTour = ({ city }) => {
     return(
         <div>
             <h2>Virtual Tour of {city.name}</h2>
-             {/*Display 360-degree panoramic images here */}
+            <PanoramaViewer
+                width="100%"
+                height="500px"
+                imageUrl={city.panoramaImage}
+                onSceneChange={sceneId => console.log('Changed to scene', sceneId)}
+                />
         </div>
     );
 };
